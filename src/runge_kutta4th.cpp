a57ad58fb960aad26b68d64c636b833663ce9665
Razvan <rva502@york.ac.k>
2019-10-22 15:54:52 +0100

    Fixed RK

--- src/runge_kutta4th.cpp
+++ src/runge_kutta4th.cpp
@@ -1,4 +1,4 @@
-// Author: Razvan Ababei	
+// Author: Razvan Ababei & Matt Ellis	
 // University of Sheffield
 // date 21st of August, 2019
 //
@@ -33,8 +33,8 @@ namespace integrate{
 	double phi_t(double dEx, double phi_rk, double H){
 		return prefac3*dEx+prefac4*sin(2*phi_rk)+ zeeman_prefac2*H;	
 	}
-	double x_t(double DWs, double phi){
-		return prefac2*sin(2*phi)*DWs + stor::alpha*DWs*phi; 
+	double x_t(double DWs, double phi, double phi_t){
+		return prefac2*sin(2*phi)*DWs + stor::alpha*DWs*phi_t; 
 	}
 	double runge_kutta(){
 		phi_k=stor::phi_dw;
@@ -45,16 +45,23 @@ namespace integrate{
                 calculate::calculate_DW(phi_k);
 
 		kp1=integrate::Dt*phi_t(stor::dEx, phi_k, stor::V);
-		k1=integrate::Dt*x_t(stor::Dw_size, phi_k);
+		k1=integrate::Dt*x_t(stor::Dw_size, phi_k, phi_t(stor::dEx, phi_k, stor::V));
+		calculate::update_energy_antinotches(x_k+0.5*k1);
+                calculate::calculate_DW(phi_k+0.5*kp1);
 
 		kp2=integrate::Dt*phi_t(stor::dEx, phi_k+0.5*kp1, stor::V);
-		k2=integrate::Dt*x_t(stor::Dw_size,phi_k+0.5*k1);
+		k2=integrate::Dt*x_t(stor::Dw_size,phi_k+0.5*kp1, phi_t(stor::dEx, phi_k, stor::V));
+		calculate::update_energy_antinotches(x_k+0.5*k2);
+                calculate::calculate_DW(phi_k+0.5*kp2);
 
 		kp3=integrate::Dt*phi_t(stor::dEx, phi_k+0.5*kp2, stor::V);
-		k3=integrate::Dt*x_t(stor::Dw_size,phi_k+0.5*k2);
+		k3=integrate::Dt*x_t(stor::Dw_size,phi_k+0.5*kp2,phi_t(stor::dEx, phi_k+0.5*kp2, stor::V));
+		calculate::update_energy_antinotches(x_k+0.5*k3);
+                calculate::calculate_DW(phi_k+0.5*kp3);
+
 
 		kp4=integrate::Dt*phi_t(stor::dEx, phi_k+kp3, stor::V);
-		k4=integrate::Dt*x_t(stor::Dw_size,phi_k+k3);
+		k4=integrate::Dt*x_t(stor::Dw_size,phi_k+kp3,phi_t(stor::dEx, phi_k+kp3, stor::V));
 		stor::vx=k4;
 		stor::phi_dt=kp4;
 		stor::phi_dw = phi_k + (1.0/6.0)*(kp1 +2*kp2+2*kp3+kp4);
